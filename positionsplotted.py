0ce806beb015911ff5f7dbfb5982e175a671a9a8
PaulPrice1 <100707314+PaulPrice1@users.noreply.github.com>
2023-07-19 09:56:17 -0400

    Update positionsplotted.py
    removed winsound Beep function

--- positionsplotted.py
+++ positionsplotted.py
@@ -1,7 +1,7 @@
 import pygame,sys,json,pickle,http.client, urllib.request, urllib.parse, urllib.error, base64,json,time
 from PIL import Image
 from pygame.locals import *
-from winsound import Beep
+#from winsound import Beep
 g=pickle.load(open("goend1.obj","rb"))
 colors={'OR':(255,128,0), 'RD':(255,0,0), 'BL':(0,0,255), 'GR':(0,255,0), 'SV':tuple(255 for i in range(3)), 'YL':(255,255,0)}
 
@@ -33,7 +33,7 @@ while True:
         except Exception as e:
             print("[Errno {0}] {1}".format(e.errno, e.strerror))
         trainpositions=json.loads(data)["TrainPositions"]
-        Beep(400,100)
+        #Beep(400,100)
         print(time.ctime())
         for position in trainpositions:
             #check if the current CircuitId is referenced in goend1.obj and if the train has an associated line color
